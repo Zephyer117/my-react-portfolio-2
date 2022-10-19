bb4d05d22d433e6891e1f6d0cb583020379b0696
Zephyer117 <114298812+Zephyer117@users.noreply.github.com>
2022-10-20 01:08:25 +0600

    Add files via upload

---
+++ src/Component/Pages/Blog/Data.js
@@ -0,0 +1,34 @@
+import Image1 from '../../Images/code-1.jpg'
+import Image2 from '../../Images/code-2.jpg'
+import Image3 from '../../Images/code-3.jpg'
+
+export const blogData = [
+  {
+    id: 1,
+    title: 'Getting A Job',
+    article:
+      'Geting a fair job is a bit of stretch but before that one should have a good amount of effort on his works',
+    linkImg: Image1,
+  },
+  {
+    id: 2,
+    title: 'The Most Casual thing',
+    article:
+      'Normal fact in coding is most of the pople just dont feel interested aafter a bit of time as the progression becomes slow So, as their interest',
+    linkImg: Image2,
+  },
+  {
+    id: 3,
+    title: 'The Best Thing to do',
+    article:
+      'One must keep on going and wroking as the time progress things will start to get clearer',
+    linkImg: Image3,
+  },
+  {
+    id: 4,
+    title: 'About Coding Fundamental',
+    article:
+      'The basics of coding is practice the more you work slowly but sruely this will get implemented in you',
+    linkImg: Image1,
+  },
+]
