8e478c5caf41e7e0a8e14c6490607ba03ab80882
Zephyer117 <114298812+Zephyer117@users.noreply.github.com>
2022-10-20 01:28:45 +0600

    Add files via upload

---
+++ src/Component/Pages/Client/ClientReview.js
@@ -0,0 +1,30 @@
+import img1 from '../../Images/code-1.jpg'
+import img2 from '../../Images/code-1.jpg'
+import img3 from '../../Images/code-1.jpg'
+
+export const clientReview = [
+  {
+    id: 1,
+    image: img1,
+    article:
+      'Sed elit quam, iaculis sed semper sit amet udin vittheae nibi at magna akal semperFusce commodo molestie  elit quam, iaculis sed sempsum Dolort tusima olatiup udin vitae magna semperFusceSed elit quam, iaculis sed semper udin vitae nibh. at magna akal semperFusce.',
+    name: 'John ',
+    occp: ' - Designer',
+  },
+  {
+    id: 2,
+    image: img2,
+    article:
+      'Sed elit quam, iaculis sed semper sit amet udin vittheae nibi at magna akal semperFusce commodo molestie  elit quam, iaculis sed sempsum Dolort tusima olatiup udin vitae magna semperFusceSed elit quam, iaculis sed semper udin vitae nibh. at magna akal semperFusce.',
+    name: 'John ',
+    occp: ' - Designer',
+  },
+  {
+    id: 3,
+    image: img3,
+    article:
+      'Sed elit quam, iaculis sed semper sit amet udin vittheae nibi at magna akal semperFusce commodo molestie  elit quam, iaculis sed sempsum Dolort tusima olatiup udin vitae magna semperFusceSed elit quam, iaculis sed semper udin vitae nibh. at magna akal semperFusce.',
+    name: 'John ',
+    occp: ' - Designer',
+  },
+]
